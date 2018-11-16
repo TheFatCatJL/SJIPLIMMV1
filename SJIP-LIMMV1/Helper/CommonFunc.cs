c94c7bf4d4ff82ab4c502ef677fb2663321a00c7
Jason Lim <yong_hui_jason@yahoo.com.sg>
2018-11-16 09:11:48 +0800

    Hope no problem

---
+++ SJIP-LIMMV1/Helper/CommonFunc.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SJIP_LIMMV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SJIP_LIMMV1.Helper
+{
+    public class CommonFunc :IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Boolean isAdminUser(IIdentity identityObj)
+        {
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(dbContext));
+            var searchForRoles = userManager.GetRoles(identityObj.GetUserId());
+            if(searchForRoles[0].ToString() == "Admin")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
