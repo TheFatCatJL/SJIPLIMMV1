30fe9201c164872ac34772883aa5877de385ae1d
Anthony <luo18chao@gmail.com>
2018-11-23 16:15:37 +0800

    added search view, controller and models

---
+++ SJIP-LIMMV1/Controllers/SearchController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using SJIP_LIMMV1.Models;
+
+namespace SJIP_LIMMV1.Controllers
+{
+    public class SearchController : Controller
+    {
+
+
+
+        // GET: Search
+        //[Route("/search/createView/{searchViewModel}")]
+        public ActionResult createView()
+        {
+
+            LiftInstallationDataDBEntities1 db = new LiftInstallationDataDBEntities1();
+            SearchViewModel searchViewModel = new SearchViewModel();
+
+
+            searchViewModel.SensorBoxInfoResults = db.SensorBoxInfoes.ToList();
+
+            return View(searchViewModel);
+        }
+        [HttpPost]
+        //[Route("/search/submitSearch")]
+        public async Task<ActionResult> submitSearch(SearchViewModel searchViewModel)
+        {
+
+            LiftInstallationDataDBEntities1 db = new LiftInstallationDataDBEntities1();
+            var sensorBoxInfo = db.SensorBoxInfoes.ToList();
+
+            if (searchViewModel.Block != null)
+            {
+                sensorBoxInfo = sensorBoxInfo.Where(x => (x.BlockNo.ToString()).StartsWith((searchViewModel.Block.ToString().Trim()))).ToList();
+            }
+            if (searchViewModel.TownCouncil != null)
+            {
+                sensorBoxInfo = sensorBoxInfo.Where(x => x.TownCouncil.ToLower().Contains(searchViewModel.TownCouncil.Trim().ToLower())).ToList();
+            }
+            if (searchViewModel.SIMCard != null)
+            {
+                sensorBoxInfo = sensorBoxInfo.Where(x => x.SIMCard.ToLower().StartsWith(searchViewModel.SIMCard.Trim().ToLower())).ToList();
+            }
+            if (searchViewModel.LMPD != null)
+            {
+                sensorBoxInfo = sensorBoxInfo.Where(x => x.LMPD.ToLower().StartsWith(searchViewModel.LMPD.Trim().ToLower())).ToList();
+            }
+
+            searchViewModel.SensorBoxInfoResults = sensorBoxInfo;
+
+
+            //return View("createView", searchViewModel);
+            return Json(searchViewModel.SensorBoxInfoResults, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
---
+++ SJIP-LIMMV1/Models/SearchViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SJIP_LIMMV1.Models
+{
+    public class SearchViewModel
+    {
+        [Required(ErrorMessage = "Please enter the name of town council")]
+        public String TownCouncil { get; set; }
+        public String SIMCard { get; set; }
+
+        [Range(0, 999, ErrorMessage = "Block number must be between 1 to 999")]
+        public int? Block { get; set; }
+        public String LMPD { get; set; }
+
+        public List<SensorBoxInfo> SensorBoxInfoResults { get; set; }
+
+        public SearchViewModel()
+        {
+            TownCouncil = null;
+            SIMCard = null;
+            Block = null;
+            LMPD = null;
+            SensorBoxInfoResults = new List<SensorBoxInfo>();
+        }
+
+
+    }
+}
