96e9ab880dd3452839b84b3ebc699d2de6bf2f16
leo10101 <30902240+leo10101@users.noreply.github.com>
2018-11-26 11:08:53 +0800

    JSON id extraction

---
+++ SJIP-LIMMV1/Controllers/ExtractFileNameController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SJIP_LIMMV1.Controllers
+{
+    public class ExtractFileNameController : Controller
+    {
+        // GET: NameExtraction
+        [HttpGet]
+        public ActionResult NameExtraction()
+        {
+            return View(new List<HttpPostedFileWrapper>());
+        }
+
+        [HttpPost]
+        public ActionResult NameExtraction(List<HttpPostedFileWrapper> postedFiles)
+        {
+            if (postedFiles != null)
+            {
+                ArrayList list = new ArrayList();
+                
+                Debug.WriteLine(postedFiles.Count());
+                foreach (var file in postedFiles)
+                {
+                    
+                    list.Add(Path.GetFileNameWithoutExtension(file.FileName));
+                }
+                
+                ViewBag.FileNames = list;
+                ViewBag.Message = postedFiles.Count() + " Files uploaded successfully.";
+            }
+
+            return View();
+        }
+    }
+}
