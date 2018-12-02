c98ce5a006b164ae6bc2d0325428e2e89fb6abf6
Anthony <luo18chao@gmail.com>
2018-12-02 14:08:13 +0800

    modified and refactored Regex code at searchViewModel

--- SJIP-LIMMV1/Models/SearchViewModel.cs
+++ SJIP-LIMMV1/Models/SearchViewModel.cs
@@ -9,16 +9,30 @@ namespace SJIP_LIMMV1.Models
 {
     public class SearchViewModel
     {
-        [RegularExpression("^\\s*[a-zA-Z]{0,50}\\s*$", ErrorMessage = "Please only enter letters")]
+        //white space at both end of the string will be ignored; only accept max 30 letter input
+        const string regExrTownCouncil = "^\\s*[a-zA-Z]{0,30}\\s*$";
+
+        //white space at both end of the string will be ignored;first 14 digits must be numbers; 15th digit must be "-" and remaining 4 digits must be number
+        const string regExrSIMCard = "^\\s*[0-9]{0,14}(-){0,1}[0,9]{0,4}\\s*$";
+
+        //white space at both end of the string will be ignored;first two digit must be letters and remainning 10 digits need to be numbers
+        const string regExrLMPD = "(^\\s*[a-zA-Z]{0,1}\\s*$)|(^\\s*[a-zA-Z]{2}[0-9]{0,10}\\s*$)";
+
+        //white space at both end of the string will be ignored; first three digits must be number ranging from 1 to 999 and fourth digit must be letter if available,
+        //string start with "0" will not be accepted
+        const string regExrBlock = "(^\\s*(?!0)[0-9]{0,2}$)|(^\\s*(?!0)[0-9]{3}[a-zA-Z]{0,1}\\s*$)";
+
+
+        [RegularExpression( regExrTownCouncil,ErrorMessage = "Please only enter letters")]
         public String TownCouncil { get; set; }
 
-        [RegularExpression("^\\s*[0-9]{0,14}(-){0,1}[0,9]{0,4}\\s*$", ErrorMessage = "Please enter SIM Card number start with 14 numbers followed with a \"-\" and followed with 4 numbers")]
+        [RegularExpression(regExrSIMCard, ErrorMessage = "Please enter SIM Card number start with 14 numbers followed with a \"-\" and followed with 4 numbers")]
         public String SIMCard { get; set; }
 
-        [Range(1, 999, ErrorMessage = "Block number must be number and between 1 to 999")]
-        public int? Block { get; set; }
+        [RegularExpression(regExrBlock, ErrorMessage = "Please enter Block number between 1 to 999 and may followed with a letter")]
+        public string Block { get; set; }
 
-        [RegularExpression("^\\s*[a-zA-Z]{0,2}[0-9]{0,10}\\s*$", ErrorMessage = "Please enter LMPD number start with 2 letters followed with 10 numbers")]
+        [RegularExpression(regExrLMPD, ErrorMessage = "Please enter LMPD number start with 2 letters followed with 10 numbers")]
         public String LMPD { get; set; }
 
         public int defaultPageNumber { get; set; }
